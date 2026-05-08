28af4b402036c755e1b7e1c97585a33402dbf5c3
arizfaiyaz <arizfaiyazwork@gmail.com>
2026-05-09 03:00:35 +0530

    added state management zustand and fixed storage and other stuff

---
+++ src/types/index.ts
@@ -0,0 +1,27 @@
+export type Bookmark = {
+  id: string
+  title: string
+  url: string
+  favicon: string
+  incognito?: boolean
+  createdAt: number  //timestamp
+}
+
+export type Board = {
+  id: string
+  name: string
+  bookmarks: Bookmark[]
+  order: number
+}
+
+export type Page = {
+  id: string
+  title: string
+  boards: Board[]
+  order: number
+}
+
+export type ArcalistState = {
+  pages: Page[]
+  activePageId: string
+}
